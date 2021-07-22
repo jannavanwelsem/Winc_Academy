51a848bacc488bb85b33873ac456057b4a5f393e
jannavanwelsem <jannavanwelsem@hotmail.com>
2021-07-22 12:28:40 +0200

    eerste JavaScript-opdracht

---
+++ JavaScript_opdracht1/index.js
@@ -0,0 +1,12 @@
+console.log("Hello Winc");
+let naam = 'Janna';
+//let if;
+console.log(naam);
+console.log(4+4);
+console.log("4"+"4");
+console.log(3-2);
+console.log(2*4);
+console.log(12/3);
+console.log(100%12);
+let leeftijd = 24;
+console.log(typeof leeftijd);
