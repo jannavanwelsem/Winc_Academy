e46664078b3c84602ae039783ff9733bdb7eb055
jannavanwelsem <jannavanwelsem@hotmail.com>
2021-07-29 16:32:29 +0200

    opdr 4 JavaScript

---
+++ Javascript_opdracht4/script.js
@@ -0,0 +1,21 @@
+const age = 18;
+const identifiesAsWoman = true;
+const driverStatus = "boB";
+
+if (age >= 18) {
+    console.log('Welkom in de kroeg!');
+} else {
+    console.log('Helaas, kom later maar terug...');
+}
+
+if (identifiesAsWoman) {
+    console.log('Koop hier je ticket -> ');
+} else {
+    console.log('Helaas, kom een andere avond maar terug...');
+}
+
+if (driverStatus == "bob"){
+    console.log('Stap maar achter het stuur');
+} else {
+    console.log('Stap maar achterin');
+}
