9658e4561a111f9384f9e0991c5087239f0baa87
jannavanwelsem <jannavanwelsem@hotmail.com>
2021-08-13 19:14:46 +0200

    opdracht JavaScript parameters en arguments Winc

--- JavaScript_opdracht8/script.js
+++ JavaScript_opdracht8/script.js
@@ -1,5 +1,5 @@
-function paintWalls(northWall, eastWall) {
-    console.log('The north wall has been painted ' + northWall + ', the east wall has been painted ' + eastWall);
+function paintWalls(color, wall) {
+    console.log('The ' + wall + ' has been painted ' + color);
 }
 
 paintWalls('red');
@@ -8,8 +8,8 @@ paintWalls();
 
 //Without an argument, the function prints 'The wall has been painted undefined'
 
-paintWalls('yellow', 'pink');
+paintWalls('yellow', 'east wall');
 
 //6. Of course the order of the arguments matters when calling the function.
-//7. If I were to switch the parameters in the function definition it would make sense if I changed both the content within () and {}.
-//8. Changing things up in calling the definition would not make sense unless you want the colors of the walls to switch.
+//7. If I were to switch the parameters in the function definition I would also have to change the content within the console.log parentheses.
+//8. If I changed everything, i.e. the content of the () and the {} of the function definition and the arguments when calling it, it would still work fine of course.
