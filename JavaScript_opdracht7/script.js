d0511a57fbd66d8615e238323399c2d2d277dffc
jannavanwelsem <jannavanwelsem@hotmail.com>
2021-08-13 17:03:43 +0200

    opdrachten JavaScript-functie Winc

---
+++ JavaScript_opdracht7/script.js
@@ -0,0 +1,28 @@
+function recipeForPizza() {
+    console.log('Gather all the ingredients for pizza');
+    console.log('Make the dough for the pizza crust');
+    console.log('Prepare the pizza toppings');
+    console.log('Divide, shape and roll out the dough');
+    console.log('Put the toppings on the dough');
+    console.log('Bake the pizza');
+    console.log('Put on some extra olive oil, salt and pepper, and enjoy!');
+}
+
+//when I run the code above in my terminal, nothing happens. I am getting the result I expected: nothing is supposed to happen yet because I didn't call the function yet.
+
+function recipeForSushi() {
+    console.log('Gather all the ingredients for sushi');
+    console.log('Prepare the sushi rice');
+    console.log('Cut the ingredients');
+    console.log('Put a sheet of the seaweed on a bamboo mat');
+    console.log('Spread out the prepared rice on the seaweed');
+    console.log('Lay down the ingredients');
+    console.log('Roll it up using the bamboo mat');
+    console.log('Cut the sushi and serve it with soy sauce, pickled ginger and wasabi.');
+}
+
+recipeForSushi();
+recipeForSushi();
+recipeForSushi();
+recipeForPizza();
+recipeForPizza();
